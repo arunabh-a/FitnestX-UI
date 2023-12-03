bf0cf16c674cf553ffd46d3e560b4bc0959f0b27
Arunabh Bhattacharya <arunabh.nd@gmail.com>
2023-12-03 17:37:15 +0530

    appBar UI completed

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
     const HomePage({super.key}); 
@@ -6,25 +7,65 @@ class HomePage extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
         return Scaffold(
-            appBar: AppBar(
-                title: const Text("Dashboard",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                    ),
+            appBar: appBar(),
+            body: const Column(
+                children: [
+                    TextField(
+                        decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Color(0xffF7F8F8),
+
+                        ),
+                    )
+                ],
+            ),
+        );
+    }
+
+    AppBar appBar() {
+    return AppBar(
+            title: const Text("Dashboard",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
                 ),
-                centerTitle: true,
-                leading: Container(
-                width: 30,
-                height: 30,
-                margin: const EdgeInsets.all(10),
+            ),
+            centerTitle: true,
+            leading: GestureDetector(
+                child: Container(
+                    alignment: Alignment.center,
+                    margin: const EdgeInsets.all(10),
                     decoration: BoxDecoration(
-                        color: Colors.black12,
+                        color: const Color(0xffF7F8F8),
                         borderRadius: BorderRadius.circular(10)
+                    ),
+                    child: SvgPicture.asset(
+                        "assets/icons/arrow-back-ios.svg",
+                        height: 20,
+                        width: 20,
                     ),
                 ),
             ),
+            actions: [
+                GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                        width: 35,
+                        alignment: Alignment.center,
+                        margin: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            color: const Color(0xffF7F8F8),
+                            borderRadius: BorderRadius.circular(10)
+                        ),
+                        child: SvgPicture.asset(
+                            "assets/icons/more-horiz.svg",
+                            height: 20,
+                            width: 20,
+                        ),
+                    ),
+                ),
+            ],
         );
     }
 }
