e851a98886f79beda8213d5726d1739c170cfbc0
Arunabh Bhattacharya <arunabh.nd@gmail.com>
2023-12-05 15:14:40 +0530

    searchField completed

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,19 +7,67 @@ class HomePage extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
         return Scaffold(
+            backgroundColor: Colors.white,
             appBar: appBar(),
-            body: const Column(
+            body: Column(
                 children: [
-                    TextField(
-                        decoration: InputDecoration(
-                            filled: true,
-                            fillColor: Color(0xffF7F8F8),
-
-                        ),
-                    )
+                    searchField(),
                 ],
             ),
         );
+    }
+
+    Container searchField() {
+        return Container(
+                        margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
+                        decoration: BoxDecoration(boxShadow: [
+                            BoxShadow(
+                                color: const Color(0xff1D1617).withOpacity(0.2),
+                                blurRadius: 40,
+                                spreadRadius: 0.0,
+                            )
+                        ]),
+                        child : TextField(
+                                decoration: InputDecoration(
+                                    filled: true,
+                                    hintText: "Search for Life",
+                                    hintStyle: const TextStyle(
+                                        color: Color(0xffDDDADA),
+                                        fontSize: 16,
+                                    ),
+                                    contentPadding: const EdgeInsets.all(15),
+                                    fillColor: const Color(0xffF7F8F8),
+                                    prefixIcon: Padding(
+                                        padding: const EdgeInsets.all(12),
+                                        child: SvgPicture.asset('assets/icons/Search.svg'),
+                                    ),
+                                    suffixIcon: SizedBox(
+                                        width: 100,
+                                        child : IntrinsicHeight (
+                                            child :Row(
+                                                mainAxisAlignment: MainAxisAlignment.end,
+                                                children: [
+                                                    const VerticalDivider(
+                                                        color: Colors.black,
+                                                        indent: 10,
+                                                        endIndent: 10,
+                                                        thickness: 0.2,
+                                                    ),
+                                                    Padding(
+                                                        padding: const EdgeInsets.all(10), 
+                                                        child: SvgPicture.asset('assets/icons/Filter.svg'),
+                                                    ),
+                                                ],
+                                            ),
+                                        ),
+                                    ),
+                                    border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(15),
+                                        borderSide: BorderSide.none
+                                    )
+                                ),
+                            ),
+                    );
     }
 
     AppBar appBar() {
